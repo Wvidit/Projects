7d685e2513c849a1c9cc7793995cc24f52957df6
Vidit <nahbro2511@gmail.com>
2025-01-10 14:38:25 +0530

    v2

--- Statistella/data_compiler.py
+++ Statistella/data_compiler.py
@@ -1,9 +1,9 @@
 
 def compile(data_ptr, fill_mode) -> str:
     if fill_mode == 'mean':
-        data_ptr.fillna(data_ptr.mean(), inplace=False).to_csv('train(mean_fill).csv', index=False)
+        data_ptr.fillna(data_ptr.mean(), inplace=True)
     elif fill_mode == 'median':
-        data_ptr.fillna(data_ptr.median(), inplace=False).to_csv('train(median_fill).csv', index=False)
+        data_ptr.fillna(data_ptr.median(), inplace=True)
     elif fill_mode == 'mode':
         data_ptr.fillna(data_ptr.mode()[0], inplace=True).to_csv('train(mode_fill).csv', index=False)
     else:
